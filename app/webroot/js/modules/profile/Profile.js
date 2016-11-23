82126f30e3dd3a856148973c4dac2ec325507a95
Angelie <angelie@gc.com>
2016-11-23 09:44:03 -0500

    add profile module

---
+++ app/webroot/js/modules/profile/Profile.js
@@ -0,0 +1,19 @@
+define(['app', 'angular', 'underscore'], function(app, angular, _)
+{
+	app.controller('TimelineController',[
+        '$scope',
+        '$timeout',
+        '$state',
+        '$stateParams',
+        '$templateCache',
+        '$q',
+        'commonService',
+        'modalService',
+        'focusService',
+        'GLOBAL',
+        'Restangular',
+        function($scope, $timeout, $state, $stateParams, $templateCache, $q, Focus, GLOBAL, Restangular) {
+        	
+        }
+	]);
+});
---
+++ app/webroot/js/modules/profile/index.js
@@ -0,0 +1,14 @@
+/**
+ * Contains all the dependecies of this module
+ */
+define([
+	
+	'./Profile',
+
+	'../../services/commonService',
+	'../../services/focusService',
+	// '../../services/modalService',
+	'../../services/blockerService',
+	// '../../services/formService',
+	// '../../directives/jwertyDirective',
+], function () {});
