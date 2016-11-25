abfa895c9a75d1c9e2bf61eacd57be945d4ad6d9
Angelie <angelie@gc.com>
2016-11-24 21:54:36 -0500

    update profile

--- app/webroot/js/modules/profile/Profile.js
+++ app/webroot/js/modules/profile/Profile.js
@@ -8,19 +8,29 @@ define(['app', 'angular', 'underscore'], function(app, angular, _)
         'Restangular',
         function($rootScope, $scope, $timeout, ProfilesModel, Restangular) {
             $scope.profile = $rootScope.loginUserProfile || {};
+            
+            $scope.isProcessing = false;
         	
         	$scope.submit = function() {
+        	    
+        	    if ($scope.isProcessing) return;
+        	    
+        	    $scope.isProcessing = true;
+        	    
+        	    
         	    if ($scope.profile.id) {
         	        ProfilesModel.one('edit').one($scope.profile.id).customPOST($scope.profile).then(function(res){
         	           // console.log(res, 'the result');
         	            $scope.profile = res.Profile;
         	            $rootScope.loginUserProfile = $scope.profile;
+        	            $scope.isProcessing = false;
         	        });
         	    } else {
         	         ProfilesModel.one('add').customPOST($scope.profile).then(function(res){
         	           //  console.log(res, 'the result');
         	             $scope.profile = res.Profile;
         	             $rootScope.loginUserProfile = $scope.profile;
+        	             $scope.isProcessing = false;
         	         });
         	    }
         	}
