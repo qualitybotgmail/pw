133845bd2033a078845810f7dfe26273c1ff53f2
daryl@webmiloo.com <daryl@webmiloo.com>
2017-03-16 12:06:20 +0800

    Include node_modules and cordova plugins

---
+++ platforms/android/assets/www/js/config.js
@@ -0,0 +1,4 @@
+angular.module('starter.config',[])
+.constant('API_URL','https://jhoncistalknote.blobby.xyz/')
+.constant('BASE_URL','https://jhoncistalknote.blobby.xyz')
+.constant('STORAGE_PREFIX','talknote_');
