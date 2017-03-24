f03fe8bdf717bee80e3a3a7972389a5a75462308
daryl@webmiloo.com <daryl@webmiloo.com>
2017-03-25 04:21:53 +0800

    Remove plugins for merging

--- platforms/android/assets/www/js/config.js
+++
@@ -1,4 +0,0 @@
-angular.module('starter.config',[])
-.constant('API_URL','https://jhoncistalknote.blobby.xyz/')
-.constant('BASE_URL','https://jhoncistalknote.blobby.xyz')
-.constant('STORAGE_PREFIX','talknote_');
