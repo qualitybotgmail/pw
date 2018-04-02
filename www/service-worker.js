623fe50084c19c9de0a2a0954e3fb8b7196ba851
Ryan Monreal <ryan@webmiloo.com>
2018-04-02 20:53:37 +0800

    Removed service worker codes.

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,21 +1,8 @@
-/*var updates = {
-	'http://localhost:8100/api/profiles/notifications_count' : {"updated":false},
-	'http://localhost:8100/api/threads.json': {"updated":false},
-	'http://localhost:8100/api/groupchats/userstogroupchat':{"updated":false},
-	'http://localhost:8100/api/groupchats.json':{"updated":false}		
-};
-*/
-var urlsToCache = ["http://localhost:8100/api/profiles/notifications_count", "http://localhost:8100/api/threads.json", "http://localhost:8100/api/groupchats/userstogroupchat", "http://localhost:8100/api/groupchats.json"];
 self.addEventListener('activate', function (event) {
-	console.log("Activate");
+
 });
 
 self.addEventListener('fetch', function (event) {
- console.log(event);
-	var urlSplitted = event.request.url.split('/');
-	var end = urlSplitted[urlSplitted.length-1];
-	console.log("RL-:"+event.request.url);
-	//console.log("'"+event.request.url+"',");
 	
 });
 
@@ -24,16 +11,5 @@ self.addEventListener('push', function (event) {
 });
 
 self.addEventListener('install', function (event) {
-	console.log("Install cache");
-	event.waitUntil(
-		caches.open('playworkv3').then(function(cache) {
-			return cache.addAll(
-				"http://localhost:8100/api/profiles/notifications_count"
-			);
-		})
-	);
+
 });
-
-
-
-
